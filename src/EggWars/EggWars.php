68d86396aea11f600a209b348172b01638024bfb
GamakCZ <gamak.mcpe@gmail.com>
2016-08-02 21:40:09 +0200

    Update EggWars.php

--- src/EggWars/EggWars.php
+++ src/EggWars/EggWars.php
@@ -4,9 +4,26 @@ namespace EggWars;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
 
 class EggWars extends PluginBase implements Listener {
 
+public $prefix = C::DARK_AQUA . "[EggWars]";
+
 public function onEnable(){
  $this->getServer->getPluginManager->registerEvents($this, $this)
- $this->getLogger->info(EggWars enabled);
+ $this->getLogger->info("EggWars enabled");
+
+public function onDisable(){
+ $this->getLogger->info("EggWars disabled");
+}
+
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+ if(!$sender instanceof Player) {
+  return;
+ }
+ if($strtolower($args[0]))
+}
