e9ece8faef8c22e7a86e5cd274b7f6e5c5cd9811
GamakCZ <gamak.mcpe@gmail.com>
2017-06-06 16:35:53 +0200

    Update EventListener.php

--- src/EggWars/Event/EventListener.php
+++ src/EggWars/Event/EventListener.php
@@ -11,18 +11,7 @@ class EventListener implements Listener {
     /** @var  EggWars */
     public $plugin;
 
-    public $setup = [];
-
     public function __construct($plugin) {
         $this->plugin = $plugin;
-    }
-
-    public function onSetup(BlockBreakEvent $event) {
-        $player = $event->getPlayer();
-        foreach ($this->setup as $name => $setup) {
-            if($name == $player->getName()) {
-
-            }
-        }
     }
 }
