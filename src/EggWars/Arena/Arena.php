76227c3d70e787a26cea4cef2f9f3a19facc7c25
GamakCZ <gamak.mcpe@gmail.com>
2017-06-06 16:36:31 +0200

    Update Arena.php

--- src/EggWars/Arena/Arena.php
+++ src/EggWars/Arena/Arena.php
@@ -11,20 +11,64 @@ class Arena {
     /** @var EggWars */
     public $plugin;
 
-    /** @var  SetupManager */
+    /** @var SetupManager */
     public $setupManager;
 
-    public $arenas = [];
+    #public $arenas = [];
+
+    /*public $arenas = [
+        "EW-1" => [
+            "players" => [
+                "GamakCZ",
+                "kaliiks,..."
+            ],
+            "status" => "ingame/lobby,...",
+            "level" => "EWLobby/map"
+        ]
+    ];*/
+
+    /** @param int $sid */
+    private $sid;
 
     /** @var Level[] */
     public $runningLevels = [];
 
     public function __construct($plugin) {
         $this->plugin = $plugin;
+        $this->arenas = $this->getData()->get("arenas");
     }
 
     public function reloadArenas() {
+        foreach ($this->arenas as $arena => $data) {
+            $level = $this->plugin->getServer()->getLevelByName($data["level"]);
+            foreach ($level->getPlayers() as $player) {
+                $player->teleport($this->plugin->getServer()->getDefaultLevel()->getSafeSpawn(),0,0);
+                $player->sendMessage(EggWars::$prefix."Reloading arenas!");
+            }
+        }
+    }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function arenaExists($name) {
+        if(in_array($name, $this->arenas)) {
+            return true;
+        }
+    }
+
+    /**
+     * @param string $name
+     */
+    public function addArena($name) {
+        $array = [$name => [
+            "players" => [],
+            "status" => "lobby",
+            "map" => ""
+        ]];
+        array_merge($this->arenas, $array);
+        $this->getData()->set("arenas", $this->arenas);
     }
 
     /**
@@ -32,7 +76,8 @@ class Arena {
      */
     public function addMap(Level $level) {
         $array = $this->getData()->get("maps");
-        $this->getData()->set("maps", array_push($array, $level->getName()));
+        array_push($array, $level->getName());
+        $this->getData()->set("maps", $array);
     }
 
     /**
@@ -51,16 +96,6 @@ class Arena {
      */
     public function levelExists($name) {
         if(is_file($this->getWorldPath()."{$name}.yml")) {
-            return true;
-        }
-    }
-
-    /**
-     * @param $arena
-     * @return bool
-     */
-    public function arenaExists($arena) {
-        if(is_file($this->getArenaPath()."{$arena}.yml")) {
             return true;
         }
     }
@@ -98,5 +133,13 @@ class Arena {
      */
     public function getArenaPath() {
         return $this->plugin->getDataFolder()."arenas/";
+    }
+
+    /**
+     * @param string $arena
+     * @return array
+     */
+    public function getArenaPlayers($arena) {
+        return $this->arenas[$arena]["players"];
     }
 }
